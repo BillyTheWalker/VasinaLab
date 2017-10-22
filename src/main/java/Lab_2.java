f79d83d57e81ddb6791277f5aa36b370a7865a5e
Volodimir Bilevych <Kishka@M-PLUS.local>
2017-10-23 01:35:08 +0300

    trying to complete

---
+++ src/main/java/Lab_2.java
@@ -0,0 +1,35 @@
+import interfaces.System_Equation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Kishka on 22.10.2017.
+ */
+public class Lab_2 {
+
+    public static void main(String[] args) {
+        System_Equation system_equation = new System_Equation();
+
+        system_equation.a.add(new ArrayList<Double>());
+        system_equation.a.get(0).add(2.68);
+        system_equation.a.get(0).add(-.68);
+        system_equation.a.get(0).add(.48);
+        system_equation.a.get(0).add(3.868);
+
+        system_equation.a.add(new ArrayList<Double>());
+        system_equation.a.get(1).add(-.73);
+        system_equation.a.get(1).add(2.92);
+        system_equation.a.get(1).add(-.39);
+        system_equation.a.get(1).add(4.329);
+
+        system_equation.a.add(new ArrayList<Double>());
+        system_equation.a.get(2).add(.58);
+        system_equation.a.get(2).add(-1.12);
+        system_equation.a.get(2).add(3.12);
+        system_equation.a.get(2).add(7.532);
+
+        system_equation.methodGauss();
+    }
+
+}
---
+++ src/main/java/interfaces/System_Equation.java
@@ -0,0 +1,51 @@
+package interfaces;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Kishka on 22.10.2017.
+ */
+public class System_Equation {
+    private static NumberFormat format = new DecimalFormat("#0.000000");
+
+    public List<Double> x = new ArrayList<Double>();
+    public List<List<Double>> a = new ArrayList<List<Double>>();
+
+    public void methodGauss() {
+        List<List<Double>> c = new ArrayList<List<Double>>();
+        for (int k = 0; k < a.size(); k++) {
+            c.add(new ArrayList<Double>());
+            for (int i = 0; i < k; i++) {
+                c.get(k).add(i, 1.0);
+            }
+            for (int i = k; i < a.get(k).size(); i++) {
+                c.get(k).add(a.get(k).get(i) / a.get(k).get(k));
+            }
+            for (int i = k + 1; i < a.size(); i++) {
+                for (int j = k + 1; j < a.get(0).size(); j++) {
+                    a.get(i).set(j, a.get(i).get(j) - a.get(i).get(k) * c.get(k).get(j));
+                }
+            }
+            for (int i = k; i < a.size();i++) {
+                a.get(i).set(k, .0);
+            }
+            for (List<Double> list : a) {
+                for (Double el : list) {
+                    System.err.print(" " + format.format(el));
+                }
+                System.err.println("");
+            }
+            System.err.println("");
+            for (List<Double> list : c) {
+                for (Double el : list) {
+                    System.err.print(" " + format.format(el));
+                }
+                System.err.println("");
+            }
+            System.err.println("");
+        }
+    }
+}
